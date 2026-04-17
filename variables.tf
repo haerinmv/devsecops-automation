7921d443347a29086d30f6f30288962e918a6e7c
haerinmv <haerin@localhost.localdomain>
2026-04-17 17:08:29 +0200

    add variable for modifications later

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  description = "region AWS"
+  type        = string
+  default     = "eu-west-3"
+}
+
+variable "instance_type" {
+  description = "Le type d'instance EC2"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "vpc_cidr" {
+  description = "Le CIDR block du VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "Le CIDR du subnet public"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "Le CIDR du subnet privé"
+  type        = string
+  default     = "10.0.2.0/24"
+}
