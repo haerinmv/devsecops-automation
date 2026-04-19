7a9b9d8cd74493c7a64ac56027ae43b2f143ebb8
haerinmv <haerinmv7@gmail.com>
2026-04-19 10:27:21 +0200

    ajout du S3

--- provider.tf
+++ provider.tf
@@ -1,10 +1,17 @@
-
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+  }
+
+  backend "s3" {
+    bucket         = "devsecops-tfstate"
+    key            = "state/terraform.tfstate"
+    region         = "eu-west-3"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
   }
 }
 
