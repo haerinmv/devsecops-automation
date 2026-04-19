81b7c7655e31930919e199c5114fd785d35e6e63
haerinmv <haerinmv7@gmail.com>
2026-04-19 11:25:38 +0200

    correction fichier

--- security.tf
+++ security.tf
@@ -3,17 +3,17 @@ resource "aws_security_group" "bastion_sg" {
   description = "Autorise SSH uniquement depuis mon IP"
   vpc_id      = aws_vpc.network1.id
 
-# seul ssh sur le port 22 est autorisé
+  # seul ssh sur le port 22 est autorisé
 
   ingress {
     description = "SSH depuis mon IP"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # On mettra ton IP précise plus tard !
+    cidr_blocks = ["0.0.0.0/0"] # On mettra ton IP précise plus tard !
   }
 
-# peut aller partout pour recuperer des maj
+  # peut aller partout pour recuperer des maj
 
   egress {
     from_port   = 0
